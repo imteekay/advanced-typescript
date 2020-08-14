d1424ac2cb01468755a9a44eb212b59747400534
TK <leandrotk100@gmail.com>
2020-08-14 19:39:38 -0300

    Add destruturing

---
+++ 001.ts
@@ -0,0 +1,23 @@
+// Destructuring Arrays
+
+const medals = ['Gold', 'Silver', 'bronze'];
+
+const first = medals[0];
+const second = medals[1];
+const third = medals[2];
+
+const [gold, silver, bronze] = medals;
+
+// Destruturing Objects
+
+const person = {
+  name: 'TK',
+  address: '123 Some Street',
+  phone: '99994828'
+};
+
+const personName = person.name;
+const personAddress = person.address;
+const personPhone = person.phone;
+
+const { name, address, phone } = person;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advanced TypeScript
+
+- [001: Destructuring](./001.ts)
