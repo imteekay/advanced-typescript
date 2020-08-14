8e8e29f5e5aa33ed8901e3473e99605bd36cac2d
TK <leandrotk100@gmail.com>
2020-08-14 20:19:19 -0300

    Union type

---
+++ 005.ts
@@ -0,0 +1,36 @@
+// Union Types
+
+type Book = {
+  title: string;
+  author: string;
+};
+
+const books: Book[] = [
+  {
+    title: 'Harry Potter 1',
+    author: 'JK Rowling'
+  },
+  {
+    title: 'Harry Potter 2',
+    author: 'JK Rowling'
+  }
+];
+
+type Paper = {
+  title: string;
+  researcher: string;
+};
+
+const papers: Paper[] = [
+  {
+    title: 'Why Functional Programming Matters',
+    researcher: 'John Hughes'
+  },
+  {
+    title: 'Out of the Tar Pit',
+    researcher: 'Ben Moseley & Peter Marks'
+  }
+];
+
+const readingMaterial: Book | Paper = books[0];
+const anotherReadingMaterial: Book | Paper = papers[0];
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - [002: Spread Operator](./002.ts)
 - [003: Tuple Type](./003.ts)
 - [004: Interface + Extends + Array + Generics + Union = Tuple](./004.ts)
+- [005: Union Type](./005.ts)
