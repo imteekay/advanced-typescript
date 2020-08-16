98c9bda8b0f29feb88699cd9ffdb781230b84560
TK <leandrotk100@gmail.com>
2020-08-16 13:28:12 -0300

    Polymorphic this

---
+++ 007.ts
@@ -0,0 +1,24 @@
+// Polymorphic this types
+
+class Vehicle {
+  drive() {
+    return this;
+  }
+}
+
+class Car extends Vehicle {
+
+}
+
+class Bus extends Vehicle {
+
+}
+
+function printThisValue(instance: Vehicle) {
+  const thisValue = instance.drive();
+  console.log(thisValue);
+}
+
+printThisValue(new Vehicle()); // Vehicle {}
+printThisValue(new Car()); // Car {}
+printThisValue(new Bus()); // Vehicle {}
