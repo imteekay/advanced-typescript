2cf70930300a5f472b7b73af0d81d5df7d04ffec
TK <leandrotk100@gmail.com>
2020-08-18 19:08:05 -0300

    Optional Chaining

---
+++ src/optional-chaining.ts
@@ -0,0 +1,15 @@
+type Payload = {
+  response?: {
+    status?: number;
+  };
+};
+
+function isTimeoutError(payload: Payload) {
+  return payload &&
+    payload.response &&
+    payload.response.status === 504;
+}
+
+function isTimeoutErrorOptionalChain(payload: Payload) {
+  return payload?.response?.status === 504;
+}
