46cd20c82ec171b7fac4fe31134557c16dd4ec22
Raphael Thomazella <thomazella9@gmail.com>
2020-08-18 19:55:35 -0300

    feat(exhaustive-switch): add .ts file

---
+++ src/exhaustive-switch.ts
@@ -0,0 +1,50 @@
+/**
+ * Here the compiler checks that we have handled all possible cases in the switch statement
+ * in other words, a compile-time exhaustive check
+ */
+
+/**
+ * This function enables the exhaustive check.
+ * It works by returning `never`, and accepting the first param as `never`
+ * @param _type The type that we're checking
+ * @param message A message to display if it errors
+ * @param lastWords A callback to call before the error
+ */
+export function assertUnreachable(
+  _type: never,
+  message?: string,
+  lastWords?: () => void
+): never {
+  if (lastWords) lastWords();
+  throw {
+    message:
+      message || "Invariant unreachable type error. Something isn't right!",
+  };
+}
+
+/**
+ * You have to write this type, it should have all possible types for the possible
+ * switch values
+ */
+type Union = "A" | "B" | "C";
+
+let value: Union;
+
+switch (value) {
+  case "A":
+    // handle 'A'
+    break;
+  case "B":
+    // handle 'B'
+    break;
+  // since C block is commented out, the code will
+  // not compile since the _type argument to assertUnreachable
+  // must a `never`
+  // case "C":
+  //   // handle 'C'
+  //   break;
+
+  default:
+    // @ts-expect-error Argument of type '"C"' is not assignable to parameter of type 'never'.ts(2345)
+    assertUnreachable(value, 'Switch has a missing clause!', () => console.log({ typeError: 'on switch' }));
+}
