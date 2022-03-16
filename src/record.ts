e1afcebf0a45382cf4ca4f946ad76b0a4967ab62
imteekay <leandrotk100@gmail.com>
2022-03-16 16:37:28 -0300

    simple record type

---
+++ src/record.ts
@@ -0,0 +1,30 @@
+// simple record type
+type AlphabetToIndex = Record<string, number>;
+
+const alphabetToIndex: AlphabetToIndex = {
+  a: 0,
+  b: 1,
+  c: 2,
+  d: 3,
+  f: 4,
+  g: 5,
+  h: 6,
+  i: 7,
+  j: 8,
+  k: 9,
+  l: 10,
+  m: 11,
+  n: 12,
+  o: 13,
+  p: 14,
+  q: 15,
+  r: 16,
+  s: 17,
+  t: 18,
+  u: 19,
+  v: 20,
+  w: 21,
+  x: 22,
+  y: 23,
+  z: 24,
+};
